c0cdbd509e736d07232d023866e1d45d70b81005
CPLUS <cplusplusplusplusplus@gmail.com>
2017-05-10 20:36:04 +0200

    Napravio osnovni model

---
+++ StudentskaSluzba/StudentskaSluzba/Model/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StudentskaSluzba
+{
+    class Student
+    {
+        internal string indeks;
+        internal double prosek;
+        internal string ime;
+        internal string prezime;
+
+        public Student(string imeArg, string prezimeArg, double prosekArg)
+        {
+            ime = imeArg;
+            prezime = prezimeArg;
+            prosek = prosekArg;
+        }
+    }
+}
+
