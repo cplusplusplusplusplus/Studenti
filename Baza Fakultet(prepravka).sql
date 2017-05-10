b45b49d586bcf2e86c93b7b4c898b4d207bf7d11
Aleksandar Grujic <astralniletac@yahoo.com>
2017-05-10 20:50:19 +0200

    izmenio fajl za bazu

--- Baza Fakultet(prepravka).sql
+++ Baza Fakultet(prepravka).sql
@@ -1,7 +1,7 @@
 
 use DotNetKurs;
 
-	drop table studenti;
+
 
 	create table fakultet (
 		student_id integer identity(1,1) primary key not null, 
@@ -11,8 +11,8 @@ use DotNetKurs;
 		prosek float	
 	);
 
-	insert into fakultet (ime, prezime, indeks, prosek) values ('Petar', 'Mihajlovic', 'E111', 'Novi Sad');
-	insert into fakultet (ime, prezime, indeks, prosek) values ('Dejan', 'Ivanovic', 'E222', 'Loznica');
-	insert into fakultet (ime, prezime, indeks, prosek) values ('Zoran', 'Kovacevic', 'E3333', 'Indjija');
-	insert into fakultet (ime, prezime, indeks, prosek) values ('Marko', 'Popovic', 'E444', 'Novi Sad');
+	insert into fakultet (ime, prezime, indeks, prosek) values ('Petar', 'Mihajlovic', 'E111', 10.0);
+	insert into fakultet (ime, prezime, indeks, prosek) values ('Dejan', 'Ivanovic', 'E222', 8.5);
+	insert into fakultet (ime, prezime, indeks, prosek) values ('Zoran', 'Kovacevic', 'E3333', 9.3);
+	insert into fakultet (ime, prezime, indeks, prosek) values ('Marko', 'Popovic', 'E444', 7.2);
 	
